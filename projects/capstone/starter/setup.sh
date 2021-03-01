34be682f36b68e7134f4e8442b53899ca2439a70
Omar <3omarthu@gmail.com>
2021-03-01 21:31:28 +0300

    up

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,5 +1,5 @@
 AUTH0_DOMAIN = 'fsndomar.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'http://localhost:8080'
-database_path = "sqlite:///C://Users//Lenovo//Documents//GitHub//Capstone//projects//capstone//starter//database.db"
+database_path = "postgres://uqheqlbdabcdsr:156d3448bb9de149d10f9d2bc5b7b5ec1483456d2be711b06710113c33443def@ec2-3-209-176-42.compute-1.amazonaws.com:5432/den9afibrkmq4o"
 database_path_test = "sqlite:///C://Users//Lenovo//Documents//GitHub//Capstone//projects//capstone//starter//database_test.db"

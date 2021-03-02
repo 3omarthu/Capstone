24bfab28de53fe82f5d65f058840fcaac2d40400
Omar <3omarthu@gmail.com>
2021-03-02 20:03:54 +0300

    Update setup.sh

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,4 +1,6 @@
 AUTH0_DOMAIN = 'fsndomar.us.auth0.com'
+AUTH0_CLIENT_ID = 'RvGhfinFKqlP2YQJ4JNaGPe7RHPdTEms'
+AUTH0_CLIENT_SECRET = '4rROHEaS9QdvJAZTldl-WJtT0vlONGCn3NEmOgvXLsMpW13NpYNd5hDMWJUuwJ-0'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'http://localhost:8080'
 database_path = "postgres://yavgsgiwsnjpis:48db92f09a67d31af2e83b1c152aca0eeb024f34a9c051596fb0e6c540d57f6a@ec2-3-232-163-23.compute-1.amazonaws.com:5432/daif2ne8fscvc9"

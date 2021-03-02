f3b1023c6f7a3191c29b3641cc142a42bd111229
Omar <3omarthu@gmail.com>
2021-03-02 19:54:14 +0300

    up

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -5,7 +5,7 @@ import json
 import os
 
 
-database_path = os.environ['DATABASE_URL']
+database_path = "postgres://yavgsgiwsnjpis:48db92f09a67d31af2e83b1c152aca0eeb024f34a9c051596fb0e6c540d57f6a@ec2-3-232-163-23.compute-1.amazonaws.com:5432/daif2ne8fscvc9"
 db = SQLAlchemy()
 
 
